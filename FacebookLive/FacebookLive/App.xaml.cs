f6518bf452ed3e629501e59b9cbb67222b29fe6f
John Patrick Quintos <113108715+patqnts@users.noreply.github.com>
2024-02-06 21:30:00 +0800

    init

---
+++ FacebookLive/FacebookLive/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FacebookLive
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
